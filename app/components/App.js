4975990c3d43d7bda6366c85ccf7aa55b81e8f65
Aleksandar Jovanov <ace92bt@gmail.com>
2016-02-08 20:32:01 +0100

    Change the structure

---
+++ app/components/App.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const App = () => (
+  <h2>Hello World</h2>
+);
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  entry: './app/components/Main.js',
+  entry: './app/index.js',
   output: {
     filename: 'public/bundle.js'       
   },
   module: {
     loaders: [
       {
-        test: /\.jsx?$/,
-        exclude: /(node_modules|bower_components)/,
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
         loader: 'babel',
         query: {
-          presets: ['react', 'es2015']
+          presets: ['react', 'es2015', 'stage-0']
         }
       }
     ]
